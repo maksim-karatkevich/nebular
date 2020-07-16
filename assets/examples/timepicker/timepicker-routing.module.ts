f2e17a27f17f06733fcc33de7dfea060a78bbfa9
Maksim Karatkevich <m.karatkevich@akveo.com>
2020-07-16 15:11:36 +0300

    Auto-generated commit

--- assets/examples/timepicker/timepicker-routing.module.ts
+++ assets/examples/timepicker/timepicker-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Route} from '@angular/router';
 import { TimepickerShowcaseComponent } from './timepicker-showcase.component';
 import { TimepickerTwelveHoursFormatComponent } from './timepicker-twelve-hours-format.component';
 import { TimepickerSingleColumnComponent } from './timepicker-single-column.component';
+import { TimepickerWithSecondsComponent } from './timepicker-with-seconds.component';
 
 const routes: Route[] = [
   {
@@ -16,6 +17,10 @@ const routes: Route[] = [
   {
     path: 'timepicker-single-column.component',
     component: TimepickerSingleColumnComponent,
+  },
+  {
+    path: 'timepicker-with-seconds.component',
+    component: TimepickerWithSecondsComponent,
   },
 ];
 
--- assets/examples/timepicker/timepicker.module.ts
+++ assets/examples/timepicker/timepicker.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { TimepickerShowcaseComponent } from './timepicker-showcase.component';
 import { TimepickerTwelveHoursFormatComponent } from './timepicker-twelve-hours-format.component';
 import { TimepickerSingleColumnComponent } from './timepicker-single-column.component';
+import { TimepickerWithSecondsComponent } from './timepicker-with-seconds.component';
 import { TimepickerRoutingModule } from './timepicker-routing.module';
 import { NbCardModule, NbTimepickerModule, NbInputModule, NbIconModule, NbFormFieldModule } from '@nebular/theme';
 import { CommonModule } from '@angular/common';
@@ -12,6 +13,7 @@ import { NbMomentDateModule } from '../../../framework/moment/moment.module';
     TimepickerShowcaseComponent,
     TimepickerTwelveHoursFormatComponent,
     TimepickerSingleColumnComponent,
+    TimepickerWithSecondsComponent,
   ],
   imports: [
     NbTimepickerModule.forRoot({isTwelveHoursFormat: false}),
